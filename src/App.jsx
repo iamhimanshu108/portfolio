0430580c8e2aafb39483c5e40549b42096fa10a4
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2024-12-15 20:56:15 +0530

    BG Change

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="overflow-x-hidden text-stone-300 antialiased ">
       <div className="fixed inset-0 -z-10">
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
       </div>
 
       <div className="container mx-auto px-8"></div>
